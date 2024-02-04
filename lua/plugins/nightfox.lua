a4b411146d4f63536ab4e07635ebe3743b2d9ee0
Charlie Cox <mail@charliecox.dev>
2024-02-04 23:49:41 +0000

    Newer minimal version from scratch

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -1,8 +1,8 @@
-return {
-	"EdenEast/nightfox.nvim",
-	lazy = false,
-	priority = 99,
-	config = function()
-		vim.cmd('colorscheme terafox')
-	end
-}
+  return {
+    "EdenEast/nightfox.nvim",
+    name="terafox",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "terafox"
+    end
+  }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,45 +1,25 @@
-local mapvimkey = require("util.keymapper").mapvimkey
-
-local config = function()
-	local telescope = require("telescope")
-	telescope.setup({
-		defaults = {
-			mappings = {
-				i = {
-					["<C-j>"] = "move_selection_next",
-					["<C-k>"] = "move_selection_previous",
-				},
-			},
-		},
-		pickers = {
-			find_files = {
-				theme = "dropdown",
-				previewer = true,
-				hidden = true,
-			},
-			live_grep = {
-				theme = "dropdown",
-				previewer = true,
-			},
-			buffers = {
-				theme = "dropdown",
-				previewer = true,
-			},
-		},
-	})
-end
-
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.3",
-	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = config,
-	keys = {
-		mapvimkey("<leader>fk", "Telescope keymaps", "Show Keymaps"),
-		mapvimkey("<leader>fh", "Telescope help_tags", "Show Help Tags"),
-		mapvimkey("<leader>ff", "Telescope find_files", "Find Files"),
-		mapvimkey("<leader>fg", "Telescope live_grep", "Live Grep"),
-		mapvimkey("<leader>fb", "Telescope buffers", "Find Buffers"),
-	},
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
