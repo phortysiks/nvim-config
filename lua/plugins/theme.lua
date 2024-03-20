2622c631262172074c0c7f386b8f388f8e17fec0
Charlie Cox <mail@charliecox.dev>
2024-03-20 00:02:23 +0000

    Enabled transparency to allow terminal background to match

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,8 +1,11 @@
-  return {
-    "folke/tokyonight.nvim",
-    name="storm",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme "tokyonight"
-    end
-  }
+return {
+	"folke/tokyonight.nvim",
+	name = "storm",
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			transparent = true,
+		})
+		vim.cmd.colorscheme("tokyonight")
+	end,
+}
