6859a3c18dd9f761b55c14e530da8df88b15101b
Charlie Cox <mail@charliecox.dev>
2024-04-28 23:41:02 +0100

    Add tmux keybind integration

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,3 +8,9 @@ vim.g.mapleader = " "
 vim.g.loaded_netwr = 1
 
 vim.keymap.set('i', 'kj', '<ESC>', { silent = true })
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l', ':wincmd l<CR>')
